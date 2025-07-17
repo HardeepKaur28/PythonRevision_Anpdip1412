7fc8cf5dc698dbf51a19fc3358b8e3c4bfabd16e
Hardeep Kaur <132670595+HardeepKaur28@users.noreply.github.com>
2025-07-17 11:13:53 +0530

    Add files via upload

---
+++ conditions Format.py
@@ -0,0 +1,22 @@
+# email = input("Enter email : ")
+# password = input("Enter password : ")
+#
+# if "@" in  email :
+#     print("Email is correct ")
+# else:
+#     print("Enter vaild email")
+#
+# if len(password) >= 8:
+#     print("Password is correct")
+# else:
+#     print("Enter vaild password")
+
+
+# user_details = {"hardeep@gmail.com " : "hard#123" , "deep" : "deep123@"}
+# id = input("Enter Your email id : ")
+# password = input("Enter password : ")
+#
+# if id in user_details and password in user_details[id] :
+#     print("Login sucessfull !!")
+# else:
+#     print("Either id or password is incorrect !!")
