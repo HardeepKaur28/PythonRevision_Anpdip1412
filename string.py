538d68d1b91e3d1534079bf958014c553c1c4c92
Hardeep Kaur <132670595+HardeepKaur28@users.noreply.github.com>
2025-07-31 11:41:38 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,16 @@
+s2 = "hello python"
+
+print(s2)
+ss = s2.capitalize()
+print(ss)
+
+ss1 = s2.upper()
+print("s2 : " , s2)
+print("ss1 : ",ss1)
+
+ss2 = s2.lower()
+print("ss2 : ",ss2)
+
+print(ss1.casefold())
+print(s2.center(len(s2)+10))
+print(s2.center(len(s2)+10,'*'))
